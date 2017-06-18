51183d7226956fb61f6c282e871eb69fadd0ad8c
user <luca131986@gmail.com>
2017-06-18 20:22:13 +0300

    API list of news Task№2

--- api/controllers/EventsController.php
+++ api/controllers/EventsController.php
@@ -5,37 +5,19 @@ use common\models\Events;
 use Yii;
 use yii\data\ActiveDataProvider;
 
+
 class EventsController extends BaseController
 {
-	public $modelClass = 'common\models\Events';
-	public function actions() {
-		$act = parent::actions();
-		$actions=[];
-		$actions['index']=$act['index'];
-		$actions ['index'] ['prepareDataProvider'] = [
-				$this,
-				'prepareDataProvider'
-		];
-		$actions['view']=$act['view'];
-		return $actions;
-	}
-	public function prepareDataProvider() {
-		$query = Events::find();
-		
-		$category_id=Yii::$app->request->get('category_id');
-		$query->andFilterWhere(['category_id'=>$category_id]);
-		
-		$from_timestamp=Yii::$app->request->get('from_timestamp');
-		$to_timestamp=Yii::$app->request->get('to_timestamp');
-		$query->andFilterWhere(['>=','event_start',$from_timestamp]);
-		$query->andFilterWhere(['<=','event_start',$to_timestamp]);
-		
-		$dataProvider = new ActiveDataProvider( [
-				'query' => $query,
-				//'sort'=> ['defaultOrder' => ['forum_id'=>SORT_DESC]]
-		] );
-	
-		return $dataProvider;
-	}
-
+    public $modelClass = 'common\models\Events';
+    public function actions() {
+        $act = parent::actions();
+        unset($act['delete']);
+        return $act;
+    }
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator']['except']=['index'];
+        return $behaviors;
+    }
 }
--- api/controllers/NewsController.php
+++ api/controllers/NewsController.php
@@ -1,32 +1,28 @@
 <?php
 namespace api\controllers;
 
-use common\models\NewsSeach;
-use common\models\Polls;
+use common\models\Events;
+use Yii;
 use yii\data\ActiveDataProvider;
 use common\models\News;
-
-
-
 
 class NewsController extends BaseController
 {
     public $modelClass = 'common\models\News';
-
     public function actions() {
         $act = parent::actions();
-        $actions=[];
-        $actions['index']=$act['index'];
-        $actions ['index'] ['prepareDataProvider'] = [
-            $this,
-            'prepareDataProvider'
-        ];
-        $actions['view']=$act['view'];
-        return $actions;
+        unset($act['delete']);
+        return $act;
+    }
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator']['except']=['index'];
+        return $behaviors;
     }
 
     public function actionIndex() {
-       $allNews = News::find()->all();
+        $allNews = News::find()->all();
         return $allNews;
     }
 
